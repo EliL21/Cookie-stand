a9d44e31872c2224e0b1a0d3602ee21e9a898a3e
eli bailey <baileyeli19@gmail.com>
2022-12-12 06:56:21 +0000

    adjustment

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 let eachStore = [];
 let table = document.getElementById('eachStore');
-let form = document.getElementById('form');
+// let form = document.getElementById('form');
 // ******** Constructor ******************
 // form.addEventListener('submit',handlesubmit);
 
